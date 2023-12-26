4dbe55556aa80af2930c46ca4d3560e72be38755
Mike Kaganski <mike.kaganski@collabora.com>
2023-12-26 20:03:10 +0600

    Handle LOK_CALLBACK_TOOLTIP in wsd
    Send "tooltip: <payload>" to client for this callback type.
    Client-side handling should be implemented separately.
    
    Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
    Change-Id: I2d1a07dd36fa20a4b5ff785da788356e0f41d62f

--- kit/ChildSession.cpp
+++ kit/ChildSession.cpp
@@ -3175,6 +3175,11 @@ void ChildSession::loKitCallback(const int type, const std::string& payload)
         sendTextFrame("corelog: " + payload);
         break;
     }
+    case LOK_CALLBACK_TOOLTIP:
+    {
+        sendTextFrame("tooltip: " + payload);
+        break;
+    }
     default:
         LOG_ERR("Unknown callback event (" << lokCallbackTypeToString(type) << "): " << payload);
     }
